ab4d4565247638f044412d3788013b7cba5cd9f0
NISCHAY HR <nischayhr11@gmail.com>
2024-10-18 15:23:32 +0530

    Updated The Schema

--- models/review.js
+++ models/review.js
@@ -2,20 +2,25 @@ const mongoose=require("mongoose");
 
 const reviewschema =new mongoose.Schema({
 
-    name:String,
-    Stars:{
+    userid:String,
+    stars:{
 
         type:Number,
         min:1,
-        max:5
+        max:5,
     },
     createdAt:{
 
         type:Date,
         default:Date.now
+    },
+    content:{
+        type : String,
+        require
     }
 });
 
 
-module.exports= mongoose.model("review",reviewschema);
+const review= mongoose.model("review",reviewschema);
 
+module.exports=review;
